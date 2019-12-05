37344d04452a9b222f487b1cb73ea3e5bdbcce65
ancues02 <ancues02@ucm.es>
2019-12-05 20:22:02 +0100

    Intentando Gezi

--- docs/Gezi.js
+++ docs/Gezi.js
@@ -2,13 +2,15 @@ import Enemy from './Enemy.js';  //esto esta aqui porque funciona
 export default class Reizen extends Enemy  {
     constructor(scene, x,y, speed, dir, points, damage, lives,  sprite){
         super(scene,x,y, speed, dir, points, damage,lives, sprite);
-        this.startMove=false;
-        this.noFloorMove=10;//para que cambie de direccion cuando no hay suelo
-        this.changeDirectionX(1);
-        this.firstTouchCeiling=true;
-        this.firstTouchWall=true;
-        this.firstTouchFloor=true;
+        //this.startMove=false;
+        //this.noFloorMove=10;//para que cambie de direccion cuando no hay suelo
+        //this.changeDirectionX(1);
+        
         this.firstTouch=true;
+        
+        this.pared=false;
+
+        this.body.setMaxVelocity(speed,speed);
         this.body.setAllowGravity(false);
         this.goDown=true;
         //probar a ponerle gravedad en Y y una velocidad en Y mayoer que la gravedad,
@@ -19,157 +21,137 @@ export default class Reizen extends Enemy  {
       }
 
     preUpdate(){
+        if(this.y<=10) this.changeDirectionY(1)
+        else if(this.y>=3100) this.changeDirectionY(-1)
+
+         this.prevY=this.y;
+        // console.log("ANtes  "+this.y)
+        // console.log("ANtes  "+this.prevY)
+         this.verticalMove()
+         this.horizontalMove();
+        // this.prevY=this.y;
+
+        // console.log(this.y)
+        // console.log(this.prevY)
         //this.body.setGravityY(-600);
         if(this.goDown){
             //this.body.setAllowGravity(true);
             this.changeDirectionY(1);
+            this.changeDirectionX(1);
+
             this.verticalMove();//lo movemos
             
             if (this.body.onFloor()) this.goDown=false;
         }
-        //this.horizontalMove();//lo movemos
-        //para cambiar de direccion si no hay suelo o encuentra un muro
-        if(this.direction.x===1) {
-            if(this.direction.y===1) {
-                this.body.angle=0;
-                this.angle=0;
+       
+        if(this.body.onCeiling()){//y tiene que ser -1
+            console.log("Entro en techo")
+            this.body.angle=180;
+            this.angle=180;
+            if(this.body.onWall()){
+                if(!this.pared){//estabas yendo por el techo hasta tocar el muro
+                    console.log("de techo paso a pared")
+                    this.changeDirectionY(1)
+                }
+                else{
+                    console.log("de pared paso a techo")
+                    if(this.direction.x===1){//se mueve a la derecha
+                        this.x-=10;
+                       
+                    }
+                    else {
+    
+                        this.x+=10;
+                    }
+                    //this.x-=10
+                    this.changeDirectionX(-this.direction.x);
+
+                }
+                console.log("hola");
+                //this.techo=false;
+                //this.changeDirectionY(1);
+
             }
-            else if(this.direction.y===-1) {
-                this.body.angle=-90;
-                this.angle=-90;
-            }
+            this.pared=false;
+
+            //this.changeDirectionY(-1);
+
+            
         }
-        else if(this.direction.x===-1) {
-            if(this.direction.y===1) {
+        else if(this.body.onFloor()){//y tiene que ser 1
+            this.body.angle=0;
+            this.angle=0;
+            console.log("ENTRO EN FLOOR")
+            if(this.body.onWall()){
+                //this.suelo=false;
+                if(this.body.onWall()){
+                    if(!this.pared){//estabas yendo por el suelo hasta tocar el muro
+                        console.log("de suelo paso a pared")
+                        this.changeDirectionY(-1)
+                    }
+                    else{
+                        console.log("de pared paso a suelo")
+                        if(this.direction.x===1){//se mueve a la derecha
+                            this.x-=10;
+                           
+                        }
+                        else {
+        
+                            this.x+=10;
+                        }
+                        this.changeDirectionX(-this.direction.x);
+    
+                    }
+                    //this.techo=false;
+                    //this.changeDirectionY(1);
+    
+                }
+
+            }
+            this.pared=false;
+            
+        }
+        else if(this.body.onWall() ){//x tiene que ser -1 o 1
+            console.log ("estoy en wall")
+            /*if (this.body.onCeiling()) this.techo=true;
+            else if (this.body.onFloor()){
+                this.suelo=true;
+            }*/ 
+            if(this.direction.x===-1){
                 this.body.angle=-270;
                 this.angle=-270;
             }
-            else if(this.direction.y===-1) {
-                this.body.angle=-180;
-                this.angle=-180;
-            }
-        }
-        if(this.body.onWall() ){
-            console.log("pared");
-            //this.firstTouch=false;
-
-            if(this.firstTouchWall){
-                //this.y-=100
-                this.firstTouchCeiling=true;
-                this.firstTouchWall=false;
-                this.firstTouchFloor=true;
-                if(! this.firstTouch){ 
-                    //console.log("HOLAAAA");     
-                  //  this.body.angle-=90;
-                   // this.angle-=90;
-                }
-                else   this.firstTouch=false;
-                
-                //this.firstTouchWall=false;
-                this.changeDirectionY(this.direction.y*-1);
-                //this.changeDirectionX(0);
-                //this.changeDirectionX(0);
-
+            else{this.body.angle=270;
+                this.angle=270;
             }
             
-            this.verticalMove()
-            this.horizontalMove();
-            if(this.body.onCeiling() || this.body.onFloor() ){
-                if (this.i===1){
-                    this.i++;
-                }
-                else {
-                    this.changeDirectionX(0);                console.log("ENTRO");//
+            this.pared=true;
+          
+            
+            
 
-                }
-            }
-
-        }
-        else if(this.body.onCeiling()){
-            this.i=1;
-            console.log("techo");
-            if(this.firstTouchCeiling){
-                this.firstTouchCeiling=false;
-                this.firstTouchWall=true;
-                this.firstTouchFloor=true;
-                if(! this.firstTouch){
-                    //console.log("HOLAAAA");     
-
-                   // this.body.angle-=90;
-                    //this.angle-=90;
-                }
-                else   this.firstTouch=false;
-                //this.changeDirectionY(0);
-                this.changeDirectionX(-1);
-
-            }
-           // if(this.body.onWall()) this.changeDirectionY(0);
-            //this.changeDirectionY(-1);
-            this.verticalMove()
-            this.horizontalMove();
-        }
-        else if(this.body.onFloor()){
-            this.i=1;
-            console.log("suelo");
-            if(this.firstTouchFloor){
-                this.firstTouchCeiling=true;
-                this.firstTouchWall=true;
-                this.firstTouchFloor=false;
-                if(! this.firstTouch){
-                    //console.log("HOLAAAA");     
-
-                    //this.body.angle-=90;
-                   // this.angle-=90;
-                }
-                else   this.firstTouch=false;
-
-                //this.changeDirectionY(-1);
-                //this.changeDirectionX(0);
-                this.changeDirectionX(1);
-                //this.changeDirectionY(0);
-
-            }
-            //if(this.body.onWall()) this.changeDirectionY(0);
-            this.verticalMove()
-            this.horizontalMove();
         }
         else{
-            console.log("HOLA");
-            if(!this.firstTouchCeiling){//estaba andando por el techo
+            console.log("ELSE")
+            if(!this.goDown){
+                if(this.direction.x===1){//se mueve a la derecha
+                    this.x-=10;
+                   
+                }
+                else {
 
-                //this.body.angle-=90;
-               // this.angle-=90;
+                    this.x+=10;
+                }
+                this.changeDirectionX(-this.direction.x)
+                //this.changeDirectionY(-this.direction.y)
             }
-            else if(!this.firstTouchWall){//estaba andando por la pared
-
-            }
-            else if(!this.firstTouchFloor){//estaba andando por el suelo
-
-            }
-            
-            //this.changeDirectionX(-1);
-            //this.changeDirectionY(0);
-
-
+            //this.changeDirectionY(0)
         }
-        console.log("direcion: "+this.direction.x+"   "+this.direction.y)
-        //this.x-=this.noFloorMove;
-        //this.changeDirectionX(-1);
-        
-        /*else{
-            this.x+=this.noFloorMove;
-        }
-        if(this.body.onFloor()===false || this.body.onWall()===true) {
-            if(this.direction.x===1){
-                this.x-=this.noFloorMove;
-                this.changeDirectionX(-1);
-            }
-            else{
-                this.x+=this.noFloorMove;
-                this.changeDirectionX(1);
-            }
-        }*/
+        //console.log(this.suelo+" "+this.techo+" "+this.pared+" ");
+
         super.colisionPlayer();    
 
     }
+
 }
+
