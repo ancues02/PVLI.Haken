f92d6cfff75f89f4284d22dcc5a30cc909033bc4
Pablo Rodriguez Bobada <prodra02@ucm.es>
2019-12-15 15:25:08 +0100

    detallando tilemap

--- docs/Pinchos.tsx
+++ docs/Pinchos.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="Pinchos" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+<tileset version="1.2" tiledversion="1.3.1" name="Pinchos" tilewidth="32" tileheight="32" tilecount="8" columns="8">
  <image source="Spike.png" width="256" height="56"/>
 </tileset>
